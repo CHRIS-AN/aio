2a52bb2b7da9e4919362218e55e0f99db192956e
MJ.Kim <kmjc6611@gmail.com>
2020-12-08 20:12:42 +0900

    Dev_Setting

---
+++ src/main/java/com/olive/aio/AioApplication.java
@@ -0,0 +1,13 @@
+package com.olive.aio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AioApplication.class, args);
+    }
+
+}
