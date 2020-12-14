93f0a2911cc8b6642f60afdcb7f13694d8ca85ab
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2020-12-14 18:21:49 +0900

    초기세팅
    기본세팅값으로 생각해주세요

--- src/main/java/com/olive/aio/accounting/AccountingController.java
+++ src/main/java/com/olive/aio/accounting/AccountingController.java
@@ -4,19 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/finance")
 @RequiredArgsConstructor
 public class AccountingController {
 
-    @GetMapping("")
+    @GetMapping("/")
     private String newPurchaseSalesTradingRegisterForm() {
-        return null;
+
+        return "index";
     }
 
-    @PostMapping("")
+    @GetMapping("/wageReport")
     private String newPurchaseSalesTradingRegisterSubmit() {
-        return  null;
+
+        return "finance/wageReport";
 
     }
 }
--- src/main/java/com/olive/aio/domain/Empl.java
+++ src/main/java/com/olive/aio/domain/Empl.java
@@ -1,7 +1,12 @@
 package com.olive.aio.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Empl {
+
+    @Id @GeneratedValue
+    private String id;
 }
